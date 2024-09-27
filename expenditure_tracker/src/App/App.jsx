599658dd7bd67b6e81948e27146b402701ecf90c
Jong Turkson <jongturkson@gmail.com>
2024-09-27 12:55:57 +0100

    Made some adjustment in the node modules as i could not use the npm start command, after editing it it worked.

--- expenditure_tracker/src/App/App.jsx
+++ expenditure_tracker/src/App/App.jsx
@@ -10,7 +10,7 @@ function App() {
     
     <Routes>
     
-      <Route  path="/" element={<WelcomePage></WelcomePage>} />
+      <Route path="/" element={<WelcomePage></WelcomePage>} />
       <Route path='/homepage' element={<HomePage></HomePage>}/>
       <Route path='/login' element={<Login></Login>} />
       <Route path='/register' element={<SignUp></SignUp>} />
