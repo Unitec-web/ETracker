70a6f8c19a875783f1446e5f847bc028f956779e
Jong Turkson <jongturkson@gmail.com>
2024-10-10 22:21:22 +0100

    Navbar completed i could not add what happens when u click on an icon

--- expenditure_tracker/src/Components/ui/NavBar.jsx
+++ expenditure_tracker/src/Components/ui/NavBar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import "./navbar.css"
-function NavBar() {
+import React from 'react';
+import './navbar.css'; // Import the CSS file for styling
+import Home from 'Assets/home.png';
+import Stats from 'Assets/stats.png';
+import Reco from 'Assets/reco.png';
+import More from 'Assets/more.png';
+const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="navbar">
+      <ul>
+        <li><a href="/"><img src={Home} alt="Home" /></a></li>
+        <li><a href="/statistics"><img src={Stats} alt="Statistics" /></a></li>
+        <li><a href="/recommendations"><img src={Reco} alt="Recommendations" /></a></li>
+        <li><a href="/more"><img src={More} alt="More" /></a></li>
+      </ul>
+    </nav>
+  );
+};
 
-export default NavBar
+export default Navbar;
