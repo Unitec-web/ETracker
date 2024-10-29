3c94c010196c21c5c8caa2626b571775c8a0f01a
Carlstorm <carlglainnyuysemo@gmail.com>
2024-10-29 15:40:52 +0100

    stuff

---
+++ expenditure_tracker/src/Components - Copy/ui/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./navbar.css"
+function NavBar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavBar
---
+++ expenditure_tracker/src/Pages/expenditure/Expenditure.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TopNavbar from '../../Components/ui/TopNavBar'
+
+function Expenditure() {
+  return (
+    <div>
+        <TopNavbar />
+      this is for the test
+    </div>
+  )
+}
+
+export default Expenditure
